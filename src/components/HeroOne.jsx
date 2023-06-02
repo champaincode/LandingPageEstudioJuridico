a0fa43ea82dc5f4a181c8ac641c38f12802b78f2
champaincode <edgarlagos.355@hotmail.com>
2023-06-02 15:02:57 -0300

    responsive movile(2)

--- src/components/HeroOne.jsx
+++ src/components/HeroOne.jsx
@@ -72,47 +72,50 @@ function HeroOne() {
         <div
           className={`carousel-item ${activeSlide === 1 ? "active" : "hidden"}`}
         >
-          <div className="hero-content flex-col h-[750px] justify-start">
-            <motion.div
-              variants={fadeIn("right", 0.5)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.3 }}
-              className="w-[90vw] lg:w-1/2 flex-col md:flex justify-center items-start "
-            >
-              <h1 className="text-4xl lg:text-6xl xl:mt-12 xl:text-6xl 2xl:text-7xl py-5 font-bold text-white font-sans">
-                Todas las <br></br> reglamentaciones al día
-              </h1>
-
-              <p className="text-1xl lg:text-2xl 2xl:text-3xl  py-5 font-lato font-regular text-white">
-                Si todavía no lograste poner en regla tus inmuebles, estás a
-                tiempo de hacerlo. Nuestro equipo profesional de abogados se
-                encargará de:
-              </p>
-              <ul className="list-disc text-white">
-                <li className="custom-disc">
-                  <p className=" text-xs mb-3 lg:text-lg  2xl:text-3xl font-lato font-regular leading-relaxed text-white">
-                    Gestionar en tiempo y forma los trámites necesarios para
-                    legalizar tus inmuebles.
-                  </p>
-                </li>
-                <li className="custom-disc">
-                  <p className="text-xs mb-3 lg:text-xl  2xl:text-3xl font-lato font-regular leading-relaxed text-white">
-                    {" "}
-                    Realizar la documentación legal para brindarte protección y
-                    tranquilidad.
-                  </p>
-                </li>
-                <li className="custom-disc">
-                  <p className=" text-xs lg:text-xl  2xl:text-3xl font-lato font-regular leading-relaxed text-white">
-                    {" "}
-                    Garantizar tus derechos como propietario.
-                  </p>
-                </li>
-              </ul>
-            </motion.div>
+          <div className="hero  lg:h-[900px] h-[180vh] w-[100%] overflow-hidden">
+            <div className="hero-content w-screen  flex flex-col-reverse sm:flex-col-reverse justify-between md:flex-row-reverse  lg:h-[800px] h-[750px]">
+              <motion.div
+                variants={fadeIn("right", 0.5)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="w-full   lg:w-full 2xl:h-3/4 xl:w-3/4"
+              >
+                <img
+                  src={ImageHero}
+                  className="h-[45vh] imagen-transparente lg:h-[80vh] md:h-[50vh] lg:w-full 2xl:h-full lg:mt-12  2xl:w-full object-cover"
+                  alt="Estudio"
+                />
+              </motion.div>
+              <motion.div
+                variants={fadeIn("left", 0.5)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="w-full   lg:w-1/2 flex flex-col md:flex justify-center items-start p-5"
+              >
+                <h1 className="text-3xl text-white lg:text-4xl sm:text-3xl md:text-5xl  xl:text-5xl 2xl:text-6xl py-5 mt-6 lg:mt-0 font-bold font-sans">
+                  Todas las <br /> reglamentaciones al día
+                </h1>
+                <h3 className="text-lg lg:text-2xl xl:text-2xl text-white font-medium font-sans">
+                  Si todavía no lograste poner en regla tus inmuebles, estás a
+                  tiempo de hacerlo. Nuestro equipo profesional de abogados se
+                  encargará de:
+                </h3>
+                <p className="text-1xl lg:text-lg py-5 font-lato font-regular text-white">
+                  Gestionar en tiempo y forma los trámites necesarios para
+                  legalizar tus inmuebles.
+                </p>
+                <p className="text-1xl lg:text-lg font-lato font-regular text-white">
+                  Realizar la documentación legal para brindarte protección y
+                  tranquilidad.
+                </p>
+                <p className="text-1xl lg:text-lg font-lato font-regular text-white">
+                  Garantizar tus derechos como propietario.
+                </p>
+              </motion.div>
+            </div>
           </div>
-          <div className="2xl:w-[400px] lg:w-[500px]"></div>
         </div>
         <button
           onClick={nextSlide}
